90822f5058870e26d3a5b389386a2689cc4708c5
Alex Klymenko <klymenko.sasha21@gmail.com>
2018-04-28 22:45:48 +0300

    first home_task

---
+++ src/index.js
@@ -0,0 +1,39 @@
+function returnFirstArgument(a) {
+  let result = a;
+  return result;
+}
+
+function sumWithDefaults(a, b = 100) {
+  let result = a + b;
+  return result;
+}
+
+function returnFnResult(fn) {
+  let result = fn();
+  return result;
+}
+
+function returnCounter(number) {
+  let num = number;
+  return function() {
+    return num++;
+  }
+}
+// let f = returnCounter(10);
+// console.log(f());
+
+function returnArgumentsArray() {
+  let args = [];
+  for (let i = 0; i < arguments.length; i++) {
+    args[i] = arguments[i];
+  }
+  return args;
+}
+
+export {
+    returnFirstArgument,
+    sumWithDefaults,
+    returnArgumentsArray,
+    returnFnResult,
+    returnCounter
+}
