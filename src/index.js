9345a65de5ac0094075c296d64b324c4926c49f8
Alex Klymenko <klymenko.sasha21@gmail.com>
2018-05-03 23:10:28 +0300

    fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,33 +1,39 @@
 function returnFirstArgument(a) {
-  let result = a;
-  return result;
+    let result = a;
+
+    return result;
 }
 
 function sumWithDefaults(a, b = 100) {
-  let result = a + b;
-  return result;
+    let result = a + b;
+
+    return result;
 }
 
 function returnFnResult(fn) {
-  let result = fn();
-  return result;
+    let result = fn();
+
+    return result;
 }
 
 function returnCounter(number = 0) {
-  let num = number;
-  return function f() {
-    return ++num;
-  }
+    let num = number;
+
+    return function f() {
+        return ++num;
+    }
 }
 // let f = returnCounter(10);
 // console.log(f());
 
 function returnArgumentsArray() {
-  let args = [];
-  for (let i = 0; i < arguments.length; i++) {
-    args[i] = arguments[i];
-  }
-  return args;
+    let args = [];
+
+    for (let i = 0; i < arguments.length; i++) {
+        args[i] = arguments[i];
+    }
+    
+    return args;
 }
 
 export {
